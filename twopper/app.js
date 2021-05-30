e00123fa7c33ed8b205f5481f41a5b77cef19599
moryw <matt@skitskat.xyz>
2021-05-29 19:31:17 -0500

    Setup pug as views engine and add relevant file/folders.

--- twopper/app.js
+++ twopper/app.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-  res.send('Hello Twoppers!!')
+  res.render('index')
 })
 
 app.listen(3000, () => console.log('Server is running!!!'))
