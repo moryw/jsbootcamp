021a9767f5fe0df72e59be0e22fe2a0d6c6ed89e
moryw <matt@skitskat.xyz>
2021-07-28 13:32:09 -0500

    Add framework for a calculator

--- node-server/dog_api.js
+++ node-server/dog_api.js
@@ -1,17 +1,20 @@
-const https = require("https")
+const https = require('https');
+
 const options = {
-  hostname: "dog.ceo",
+  hostname: 'dog.ceo',
   port: 443,
-  path: "/api/breeds/list/all",
-  method: "GET",
-}
+  path: '/api/breeds/list/all',
+  method: 'GET',
+};
 
 const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`)
+  console.log(`statusCode: ${res.statusCode}`);
 
-  res.on("data", (d) => {
+  res.on('data', (d) => {
     process.stdout.write(d)
-    //note: we use process.stdout.write() here, and not console.log(), because we are receiving binary data from a stream, in chunks, and we stream each chunk to `stdout`, so the user (you) can see it.
+    // note: we use process.stdout.write() here, and not console.log(),
+    // because we are receiving binary data from a stream,
+    // in chunks, and we stream each chunk to `stdout`, so the user (you) can see it.
   })
 })
 
